e0bd0709b72c9a10b5ec32dcd89add4c9a2682a0
Nadia <nadsyif949@gmail.com>
2026-07-25 23:21:25 +0700

    feat(gallery): update activity documentation list and captions

--- src/data/galeri.ts
+++ src/data/galeri.ts
@@ -14,34 +14,69 @@ export const galeriList: FotoGaleri[] = [
     id: "galeri-1",
     src: "/images/Penerjunan.jpg",
     alt: "Penerjunan Tim KKN-T 141 Undip di Balai Desa Batursari",
-    caption: "Kegiatan penerjunan resmi Tim KKN-Tematik ESDM KKNT-141 Universitas Diponegoro bersama perangkat desa di Balai Desa Batursari.",
+    caption:
+      "Kegiatan penerjunan resmi Tim KKN-Tematik ESDM KKNT-141 Universitas Diponegoro bersama perangkat desa di Balai Desa Batursari.",
   },
   {
     id: "galeri-2",
     src: "/images/SowanRT.jpg",
     alt: "Koordinasi dan sowan ke jajaran RT dan RW Desa Batursari",
-    caption: "Momen koordinasi awal dan sowan kewilayahan bersama ketua RT dan RW setempat guna memetakan kondisi serta kebutuhan energi masyarakat desa.",
+    caption:
+      "Momen koordinasi awal dan sowan kewilayahan bersama ketua RT dan RW setempat guna memetakan kondisi serta kebutuhan energi masyarakat desa.",
   },
   {
     id: "galeri-3",
     src: "/images/Senam.jpg",
     alt: "Kegiatan senam sehat bersama ibu-ibu RT di Desa Batursari",
-    caption: "Meningkatkan keakraban dan kebersamaan melalui kegiatan senam sehat bersama ibu-ibu RT setempat guna menjaga kebugaran jasmani warga Desa Batursari.",
+    caption:
+      "Meningkatkan keakraban dan kebersamaan melalui kegiatan senam sehat bersama ibu-ibu RT setempat guna menjaga kebugaran jasmani warga Desa Batursari.",
   },
   {
     id: "galeri-4",
     src: "/images/POC.jpg",
     alt: "Proses pembuatan dan produksi Pupuk Organik Cair (POC) Desa Batursari",
-    caption: "Proses pengolahan dan pencampuran bahan baku dalam pembuatan Pupuk Organik Cair (POC) secara mandiri guna mengoptimalkan potensi limbah organik di Desa Batursari.",
+    caption:
+      "Proses pengolahan dan pencampuran bahan baku dalam pembuatan Pupuk Organik Cair (POC) secara mandiri guna mengoptimalkan potensi limbah organik di Desa Batursari.",
+    linkNews: {
+      url: "#", // TODO: Diisi link reportase Multidisiplin 1 (Esther dkk)
+      label: "JatengVox",
+    },
   },
   {
     id: "galeri-5",
     src: "/images/SosialisasiSMP3.jpg",
     alt: "Sosialisasi Transisi Hijau di SMP Negeri 3 Mranggen",
-    caption: "Edukasi dan sosialisasi transisi hijau oleh Tim KKNT-141 Undip guna mencetak generasi muda yang peduli terhadap kelestarian lingkungan di SMP Negeri 3 Mranggen.",
+    caption:
+      "Edukasi dan sosialisasi transisi hijau oleh Tim KKNT-141 Undip guna mencetak generasi muda yang peduli terhadap kelestarian lingkungan di SMP Negeri 3 Mranggen.",
     linkNews: {
       url: "https://jatengvox.com/cetak-generasi-peduli-lingkungan-kknt-141-undip-hadirkan-sosialisasi-transisi-hijau-di-smp-negeri-3-mranggen/",
-      label: "Baca berita selengkapnya di JatengVox",
+      label: "JatengVox",
     },
   },
+  {
+    id: "galeri-6",
+    src: "/images/SurveiBiogas.jpg",
+    alt: "Survei Geologi dan Pemetaan Potensi Biogas Kandang Ternak",
+    caption:
+      "Kunjungan lapangan dan survei geologi di kandang komunal warga untuk analisis kesesuaian lahan serta rancangan instalasi digester biogas.",
+    linkNews: {
+      url: "#", // TODO: Diisi link reportase Biogas (Iqbal dkk)
+      label: "JatengVox",
+    },
+  },
+    {
+    id: "galeri-7",
+    src: "/images/UMKMRoti.jpg",
+    alt: "Kunjungan dan Membantu Pengemasan Produk UMKM Roti Desa Batursari",
+    caption:
+      "Kunjungan silaturahmi sekaligus ikut membantu proses pengemasan roti di salah satu UMKM lokal Desa Batursari yang memproduksi bahan baku roti bakar.",
+  },
+  {
+    id: "galeri-8",
+    src: "/images/NgajarMasjidDaleman.JPG", 
+    alt: "Bimbingan Belajar Anak TK dan SD di Masjid Daleman Desa Batursari",
+    caption:
+      "Kegiatan bimbingan belajar dasar serta pendampingan edukatif bagi anak-anak usia TK dan SD yang dilaksanakan pada hari Jumat di Masjid Itikaf Darul Mutaqin , Desa Batursari.",
+  },
+
 ];
