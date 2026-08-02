b80b36e5323930e48140bd4ff1397c6947ba6a9a
RDN14 <ardanfadli115@gmail.com>
2026-08-02 21:30:46 +0700

    fix: penekanan keyword Multi menjadi Multi Disiplin

--- src/data/program.ts
+++ src/data/program.ts
@@ -7,7 +7,7 @@ export interface ProgramCard {
 
 export const programCards: ProgramCard[] = [
   {
-    id: "multi-1",
+    id: "multi disiplin-1",
     title: "Audit Energi Sederhana & Pemetaan EBT Lokal",
     iconSvgPath:
       "M12 2v4M12 18v4M4.93 4.93l2.83 2.83M16.24 16.24l2.83 2.83M2 12h4M18 12h4M4.93 19.07l2.83-2.83M16.24 7.76l2.83-2.83",
@@ -18,7 +18,7 @@ export const programCards: ProgramCard[] = [
     ],
   },
   {
-    id: "multi-2",
+    id: "multi disiplin-2",
     title: "Pembentukan Tim Hemat Energi & Kelembagaan",
     iconSvgPath:
       "M17 21v-2a4 4 0 0 0-4-4H7a4 4 0 0 0-4 4v2M11 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0ZM23 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75",
