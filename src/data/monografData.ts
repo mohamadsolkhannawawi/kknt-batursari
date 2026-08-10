d711d9bb424ffeb4d6df40f296128491c525745b
Nadia <nadsyif949@gmail.com>
2026-08-10 22:49:26 +0700

    feat: update monograf link to google docs document

--- src/data/monografData.ts
+++ src/data/monografData.ts
@@ -1,7 +1,7 @@
 export const monografData = {
   judul: "Potensi Biogas Berbasis Limbah Peternakan",
   subjudul: "Buku Monograf Komprehensif Pengelolaan Limbah Ternak Menjadi Energi Terbarukan Berkelanjutan",
-  draftUrl: "https://drive.google.com/your-monograph-draft-link", // <-- Ganti link draft buku kamu di sini!
+  draftUrl: "https://docs.google.com/document/d/1Po1DcqkU4DjtK0UGIbq6AtLkgJThVZyg/edit?usp=sharing&ouid=115210097609033305494&rtpof=true&sd=true", 
   // PARAGRAF KESIMPULAN / DESKRIPSI BUKU
   kesimpulan: [
     "Buku monograf ini disusun sebagai acuan komprehensif mengenai potensi pengembangan energi terbarukan berbasis limbah peternakan di Desa Batursari. Melalui kajian mendalam dari Bab I hingga Bab XII, buku ini mengulas secara holistik mulai dari latar belakang kebutuhan energi bersih, konsep dasar fermentasi anaerob, hingga karakteristik limbah ternak sapi dan kambing yang melimpah di kawasan pedesaan.",
